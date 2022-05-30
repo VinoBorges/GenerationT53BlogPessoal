287aed7434de25bf11740555e9cf16716770d8aa
VINICIOS BORGES DE LIMA <vinicios.borges02@gmail.com>
2022-05-30 17:57:15 -0300

    CRUD Postagem

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -2,10 +2,18 @@ package com.generation.blogpessoal.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,9 +29,51 @@ public class PostagemController {
 	private PostagemRepository postagemRepository;
 	
 	@GetMapping
-	public ResponseEntity<List<Postagem>> getAll(){ //Response Entity porque devolve um status http, lista de postagens
+	public ResponseEntity<List<Postagem>> getAll(){ //Response Entity porque devolve um status http, lista de postagens, cada getMapping é um caminho único que conta a partir de "postagens", cada método é único
 		return ResponseEntity.ok(postagemRepository.findAll());
 	
-		//Método a equivalente a select * from tb_postagens;
+		//Método equivalente a select * from tb_postagens;
+	}
+
+	@GetMapping("/{id}") //dentro das chaves é considerado uma variável
+	public ResponseEntity<Postagem> getById(@PathVariable Long id){
+		return postagemRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta)) //Função Lambda
+				.orElse(ResponseEntity.notFound().build());
+		
+		//select * from tb_postagens where id = id;
+	}
+	
+	@GetMapping("/titulo/{titulo}") //criando um novo caminho para depois colocar a variável para não confundir o usuario
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+	
+		//select * from tb_postagens where titulo like "%titulo%";;
+	}
+	
+	@PostMapping
+	public ResponseEntity<Postagem> postPostagem(@Valid @RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Postagem> putPostagem(@Valid @RequestBody Postagem postagem){
+		if(postagem.getId() == null)
+			return ResponseEntity.badRequest().build();
+		
+		return	postagemRepository.findById(postagem.getId())
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem)))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deletePostagem(@PathVariable Long id) {
+		
+		return postagemRepository.findById(id)
+				.map(resposta -> {
+					postagemRepository.deleteById(id);
+					return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+				})
+				.orElse(ResponseEntity.notFound().build());
 	}
 }
